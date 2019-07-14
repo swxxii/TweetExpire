9657bd6adb318cbcb8e0f59b56396b286d322f10
swxxii <did-wojg@ottermail.app>
2019-07-14 13:31:28 +1000

    initial code & readme

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # TweetExpire
-A python script to delete your tweets after a certain age
+
+A python script to delete your tweets once they reach a certain age (number of days old). I run it as a cron job every night.
+
+The Twitter API only returns max 3200 tweets. This script may not be suitable if you want to delete really old tweets past this limit. For that, try [TweetDeleter](https://github.com/Guerillero/TweetDeleter) by Guerillero.
+
+## Getting started
+
+1. [Register a Twitter App](https://developer.twitter.com/en/apps) and get the API keys.
+2. Install Python and tweepy. I use Python 3.\
+    `pip3 install tweepy`
+3. Enter configuration info in the script.
+4. Run the script `./TweetExpire.py`
+
+## Acknowledgements
+
+Based on [TweetDeleter](https://github.com/Guerillero/TweetDeleter) by Guerillero.
---
+++ TweetExpire.py
@@ -0,0 +1,70 @@
+#! /usr/bin/python3
+import tweepy
+from datetime import datetime, timedelta, timezone
+
+##########################################################################
+# CONFIGURATION
+##########################################################################
+
+# Your twitter handle
+username = ""
+
+# Delete tweets once they are this many days old
+delete_after = 180
+
+# ID's of protected tweets to keep
+protected = [1234567890987654321, 987654321234567890]
+
+# Set this to True to print skipped tweets to stdout
+verbose = False
+
+# Twitter API info. Register an app at https://developer.twitter.com/en/apps
+api_key = ""
+api_secret = ""
+access_token_key = ""
+access_token_secret = ""
+
+##########################################################################
+# MAIN SCRIPT
+##########################################################################
+
+print("[ INIT ] TweetExpire script started for @{:s}.".format(username))
+print("[ INIT ] About to delete tweets older than {:d} days.".format(
+    delete_after))
+
+
+def deleter(tw, idNo):
+    if tw.user.screen_name != username:
+        if tw.retweeted == True:
+            api.unretweet(idNo)
+    else:
+        api.destroy_status(idNo)
+
+
+auth = tweepy.OAuthHandler(api_key, api_secret)
+auth.set_access_token(access_token_key, access_token_secret)
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+count = 0
+dels = 0
+unrt = 0
+
+for tw in tweepy.Cursor(api.user_timeline, id=username).items():
+    count += 1
+    text = tw.text.replace("\r\n", " ").replace(
+        "\r", " ").replace("\n", " ").replace("  ", " ")[:80]+"..."
+    age = (datetime.now() - tw.created_at).days
+    sage = "("+str(age)+") "
+    if tw.id in protected:
+        if verbose:
+            print("[ KEEP ] "+tw.created_at.strftime("%Y-%m-%d ")+sage+text)
+    if age >= delete_after:
+        print("[DELETE] "+tw.created_at.strftime("%Y-%m-%d ")+sage+text)
+        deleter(tw, tw.id)
+        dels += 1
+    else:
+        if verbose:
+            print("[ SKIP ] "+tw.created_at.strftime("%Y-%m-%d ")+sage+text)
+
+print("[ DONE ] Processed {:d} tweets: {:d} deleted.".format(
+    count, dels))
